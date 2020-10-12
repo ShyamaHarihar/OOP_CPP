37db24ffe040d5b0b4f6d46be7a67a078a540fb2
shyama-ui <59372076+shyama-ui@users.noreply.github.com>
2020-10-13 01:15:46 +0530

    Add files via upload

---
+++ base class pointer Derived Class Object.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class Base{
+	public:
+		void fun1()
+		{
+			cout<<"fun1 of Base"<<endl;
+		}
+};
+class Derived: public Base{
+	public:
+		void fun2()
+		{
+			cout<<"fun2 of Derived"<<endl;
+		}
+};
+int main()
+{
+	Base *b;
+	b= new Derived();
+	b->fun1();	
+}
