774096604f4bf4e301df6b48d5d04f1c5f05d27f
shyama-ui <59372076+shyama-ui@users.noreply.github.com>
2020-10-12 10:32:11 +0530

    Add files via upload

---
+++ operator + overloading.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Complex{
+ public:
+ int real;
+ int imag;
+ public:
+ Complex(int r=0,int i=0)
+ {
+     real=r;
+     imag=i;
+ }
+ Complex operator+(Complex x)
+     {
+        Complex temp;
+        temp.real=real+x.real;
+        temp.imag=imag+x.imag;
+        return temp;
+     }
+ 
+};
+int main()
+{
+   Complex c1(3,7);
+   Complex c2(5,4);
+   Complex c3;
+   c3=c1 + c2;
+   cout<<c3.real<<" + "<<c3.imag<<"i"<<endl;
+}
+
