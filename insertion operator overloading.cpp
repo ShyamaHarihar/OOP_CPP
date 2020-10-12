94aa7d6455987b119405c4adeb730b7f0f9bedc8
shyama-ui <59372076+shyama-ui@users.noreply.github.com>
2020-10-12 23:59:44 +0530

    Update insertion operator overloading.cpp

--- insertion operator overloading.cpp
+++ insertion operator overloading.cpp
@@ -20,3 +20,16 @@ int main()
 	Complex c1(10,5);
 	operator<<(cout,c1);
 }
+
+/*Complex operator+(const Complex& x,const Complex& y)
+{
+    Complex temp;
+    temp.a=x.a+y.a;
+    temp.b=x.b+y.b;
+    return temp;
+}
+ostream & operator<< (ostream& os,const Complex& c1)
+{
+   return  os<<c1.a<<"+i"<<c1.b<<endl;
+}
+*/
